5c934a6b5e418aae4d9c00defd76e4d118b69fef
chobolevel <rodaka123@naver.com>
2023-12-02 17:57:53 +0900

    Feat: add user authenticated logic

--- src/main/java/com/ikea/security/UserDetailsServiceImpl.java
+++ src/main/java/com/ikea/security/UserDetailsServiceImpl.java
@@ -1,16 +1,28 @@
 package com.ikea.security;
 
+import com.ikea.entity.user.User;
+import com.ikea.mapper.user.UserMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserMapper userMapper;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return null;
+    User findUser = userMapper.findOne(User.builder().username(username).build());
+
+    if(findUser == null) {
+      throw new UsernameNotFoundException("회원정보를 찾을 수 없습니다.");
+    }
+
+    return new CustomUserDetails(findUser);
   }
 
 }
