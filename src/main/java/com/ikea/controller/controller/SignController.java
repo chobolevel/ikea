869b75a6349e839e7642adac1f5d2e6489eadd91
chobolevel <rodaka123@naver.com>
2023-12-06 20:23:48 +0900

    Feat: add sign/in, sign/up page

---
+++ src/main/java/com/ikea/controller/controller/SignController.java
@@ -0,0 +1,21 @@
+package com.ikea.controller.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sign")
+public class SignController {
+
+  @GetMapping("up")
+  public String signUp() {
+    return "/sign/up";
+  }
+
+  @GetMapping("in")
+  public String signIn() {
+    return "/sign/in";
+  }
+
+}
