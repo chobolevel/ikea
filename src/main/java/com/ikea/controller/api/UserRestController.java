be7e8954b9f5dfae46cd5b48ae60a70f0abd96bc
chobolevel <rodaka123@naver.com>
2023-12-07 16:55:15 +0900

    Feat: add checking my info api

--- src/main/java/com/ikea/controller/api/UserRestController.java
+++ src/main/java/com/ikea/controller/api/UserRestController.java
@@ -3,10 +3,12 @@ package com.ikea.controller.api;
 import com.ikea.common.BaseResponse;
 import com.ikea.entity.user.User;
 import com.ikea.exception.ApiException;
+import com.ikea.security.user.CustomUserDetails;
 import com.ikea.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +17,13 @@ import org.springframework.web.bind.annotation.*;
 public class UserRestController {
 
   private final UserService userService;
+
+  @GetMapping("me")
+  public ResponseEntity<?> getMyUser(@AuthenticationPrincipal CustomUserDetails userDetails) throws ApiException {
+    String id = userDetails.getUser().getId();
+    User findUser = userService.findOne(User.builder().id(id).build());
+    return new ResponseEntity<>(findUser, HttpStatus.OK);
+  }
 
   @PutMapping("{id}")
   public ResponseEntity<?> modifyUser(@PathVariable("id") String id, @RequestBody User user) throws ApiException {
