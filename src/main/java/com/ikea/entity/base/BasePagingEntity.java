548885e4e95404908880d23e7949643061fbe4b5
chobolevel <rodaka123@naver.com>
2023-12-11 21:48:07 +0900

    Refactor: BasePagingEntity field
    pageNum, offset, limit 쓰기 전용 필드 설정

--- src/main/java/com/ikea/entity/base/BasePagingEntity.java
+++ src/main/java/com/ikea/entity/base/BasePagingEntity.java
@@ -1,5 +1,6 @@
 package com.ikea.entity.base;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +10,11 @@ public class BasePagingEntity {
 
   private String createDate;
   private String updateDate;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   private int pageNum;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   private int offset;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   private int limit;
 
 }
