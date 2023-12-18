a837684df231717e412e4c93a641a4f9e06210f7
chobolevel <rodaka123@naver.com>
2023-12-18 14:30:43 +0900

    Refactor: add productOptionModify btn and page

--- src/main/java/com/ikea/controller/controller/ProductOptionController.java
+++ src/main/java/com/ikea/controller/controller/ProductOptionController.java
@@ -1,19 +1,33 @@
 package com.ikea.controller.controller;
 
+import com.ikea.entity.product.ProductOption;
+import com.ikea.exception.ApiException;
+import com.ikea.service.product.ProductOptionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("product-option")
+@RequiredArgsConstructor
 public class ProductOptionController {
 
-  @GetMapping("register")
-  public String registerProductOption(@RequestParam("productId") String productId, Model model) {
+  private final ProductOptionService productOptionService;
+
+  @GetMapping("/register/{id}")
+  public String registerProductOption(@PathVariable("id") String productId, Model model) {
     model.addAttribute("productId", productId);
     return "/product/option/register";
+  }
+
+  @GetMapping("/modify/{id}")
+  public String modifyProductOption(@PathVariable("id") String id, Model model) throws ApiException {
+    ProductOption findProductOption = productOptionService.findOne(ProductOption.builder().id(id).build());
+    model.addAttribute("productOption", findProductOption);
+    return "/product/option/modify";
   }
 
 }
