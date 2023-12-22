9959a68ac848b8ae7383b4cedb8a613ad1d5002e
chobolevel <rodaka123@naver.com>
2023-12-22 22:24:46 +0900

    Feat: add subCategoryListPage

--- src/main/java/com/ikea/controller/controller/CategoryController.java
+++ src/main/java/com/ikea/controller/controller/CategoryController.java
@@ -1,14 +1,19 @@
 package com.ikea.controller.controller;
 
 import com.ikea.entity.category.MainCategory;
+import com.ikea.entity.category.SubCategory;
 import com.ikea.exception.ApiException;
 import com.ikea.service.category.MainCategoryService;
+import com.ikea.service.category.SubCategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("category")
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CategoryController {
 
   private final MainCategoryService mainCategoryService;
+  private final SubCategoryService subCategoryService;
 
   @GetMapping("/main/register")
   public String mainCategoryRegister() {
@@ -36,6 +42,13 @@ public class CategoryController {
   @GetMapping("/sub/register")
   public String subCategoryRegister() {
     return "category/sub/register";
+  }
+
+  @GetMapping("/sub/list")
+  public String subCategoryList(@RequestParam("mainCategoryId") String mainCategoryId, Model model) throws ApiException {
+    List<SubCategory> subCategoryList = subCategoryService.findAll(SubCategory.builder().mainCategoryId(mainCategoryId).build());
+    model.addAttribute("subCategoryList", subCategoryList);
+    return "category/sub/list";
   }
 
 }
