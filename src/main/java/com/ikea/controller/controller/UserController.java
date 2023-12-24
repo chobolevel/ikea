993eb4c941208a1b46fc5cb21e616a12337e59dc
chobolevel <rodaka123@naver.com>
2023-12-24 20:41:50 +0900

    Refactor: modify profilePageStructure

--- src/main/java/com/ikea/controller/controller/UserController.java
+++ src/main/java/com/ikea/controller/controller/UserController.java
@@ -1,15 +1,27 @@
 package com.ikea.controller.controller;
 
+import com.ikea.entity.user.User;
+import com.ikea.exception.ApiException;
+import com.ikea.security.user.CustomUserDetails;
+import com.ikea.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
+@RequiredArgsConstructor
 public class UserController {
 
+  private final UserService userService;
+
   @GetMapping("profile")
-  public String profile() {
+  public String profile(@AuthenticationPrincipal CustomUserDetails customUserDetails, Model model) throws ApiException {
+    User user = userService.findOne(User.builder().id(customUserDetails.getUser().getId()).build());
+    model.addAttribute("user", user);
     return "user/profile";
   }
 
